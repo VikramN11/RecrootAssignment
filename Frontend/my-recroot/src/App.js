a3c2b27e8943e1b7e3ed50871a7689239714b6d3
Vikram Navik <vikramnavik98@gmail.com>
2023-07-13 00:38:34 +0530

    signup signin done

--- Frontend/my-recroot/src/App.js
+++ Frontend/my-recroot/src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import Signup from './Component/Signup';
+import Signin from './Component/Signin';
 
 function App() {
   return (
     <div className="App">
-      
+      <Signup />
     </div>
   );
 }
--- Frontend/my-recroot/src/index.js
+++ Frontend/my-recroot/src/index.js
@@ -3,13 +3,15 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {Provider} from "react-redux";
+import { ChakraProvider } from '@chakra-ui/react'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Provider store={}>
-      <App />
-    </Provider>
+      <ChakraProvider>
+        <App />
+      </ChakraProvider>
   </React.StrictMode>
 );
 
