0aff3560248d0c55e990840bf83d423fc31d3312
Vikram Navik <vikramnavik98@gmail.com>
2023-07-13 11:55:16 +0530

    handling error

--- Frontend/my-recroot/src/Component/Signup.jsx
+++ Frontend/my-recroot/src/Component/Signup.jsx
@@ -1,96 +1,59 @@
-import {
-    Flex,
-    Box,
-    FormControl,
-    FormLabel,
-    Input,
-    InputGroup,
-    HStack,
-    InputRightElement,
-    Stack,
-    Button,
-    Heading,
-    Text,
-    useColorModeValue,
-    Link,
-  } from '@chakra-ui/react';
   import { useState } from 'react';
-  import { ViewIcon, ViewOffIcon } from '@chakra-ui/icons';
+  import axios from "axios";
   
   const Signup = ()=>{
-    const [showPassword, setShowPassword] = useState(false);
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [profile, setProfile] = useState(null);
+
+    const handleSubmit = (e)=>{
+       e.preventDefault();
+       let payload = {name, email, password, profile};
+       axios.post(`http://localhost:8080/users/register`, payload).then(res=>{
+        console.log(res);
+       }).catch(err=>{
+        console.log(err)});
+    }
   
+    
     return (
-      <Flex
-        minH={'100vh'}
-        align={'center'}
-        justify={'center'}
-        bg={useColorModeValue('gray.50', 'gray.800')}>
-        <Stack spacing={8} mx={'auto'} maxW={'lg'} py={12} px={6}>
-          <Stack align={'center'}>
-            <Heading fontSize={'4xl'} textAlign={'center'}>
-              Sign up
-            </Heading>
-            <Text fontSize={'lg'} color={'gray.600'}>
-              to enjoy all of our cool features ✌️
-            </Text>
-          </Stack>
-          <Box
-            rounded={'lg'}
-            bg={useColorModeValue('white', 'gray.700')}
-            boxShadow={'lg'}
-            p={8}>
-            <Stack spacing={4}>
-              
-                  <FormControl id="name">
-                    <FormLabel>Name</FormLabel>
-                    <Input type="text" />
-                  </FormControl>
-              <FormControl id="email" isRequired>
-                <FormLabel>Email address</FormLabel>
-                <Input type="email" />
-              </FormControl>
-              <FormControl id="password" isRequired>
-                <FormLabel>Password</FormLabel>
-                <InputGroup>
-                  <Input type={showPassword ? 'text' : 'password'} />
-                  <InputRightElement h={'full'}>
-                    <Button
-                      variant={'ghost'}
-                      onClick={() =>
-                        setShowPassword((showPassword) => !showPassword)
-                      }>
-                      {showPassword ? <ViewIcon /> : <ViewOffIcon />}
-                    </Button>
-                  </InputRightElement>
-                </InputGroup>
-              </FormControl>
-              <FormControl  action="/users/register" method="POST" enctype="multipart/form-data">
-              <FormLabel>Upload Profile Picture</FormLabel>
-                <Input type='file' name="profileImage" />
-                <Button>Upload</Button>
-              </FormControl>
-              <Stack spacing={10} pt={2}>
-                <Button
-                  loadingText="Submitting"
-                  size="lg"
-                  bg={'blue.400'}
-                  color={'white'}
-                  _hover={{
-                    bg: 'blue.500',
-                  }}>
-                  Sign up
-                </Button>
-              </Stack>
-              <Stack pt={6}>
-                <Text align={'center'}>
-                  Already a user? <Link color={'blue.400'}>Login</Link>
-                </Text>
-              </Stack>
-            </Stack>
-          </Box>
-        </Stack>
-      </Flex>
+      <form method='post' encType="multipart/form-data" onSubmit={handleSubmit}>
+      <input
+        type="text"
+        name="name"
+        placeholder="Name"
+        value={name}
+        onChange={e=>setName(e.target.value)}
+        required
+      /><br />
+      <input
+        type="email"
+        name="email"
+        placeholder="Email"
+        value={email}
+        onChange={e=>setEmail(e.target.value)}
+        required
+      /><br />
+      <input
+        type="password"
+        name="password"
+        placeholder="Password"
+        value={password}
+        onChange={e=>setPassword(e.target.value)}
+        required
+      /><br />
+      <input
+        type="file"
+        name="profileImage"
+        onChange={(e)=>{
+          console.log(e.target.files[0]);
+          setProfile(e.target.files[0].name)
+        }}
+        required
+      /><br />
+      <button type="submit">Sign Up</button>
+    </form>
     );
   }
 
